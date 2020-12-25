6e3aeaf0ec879d24fd586925f4ebcf39cdfddb0d
Yee <17788108425@163.com>
2020-12-25 15:27:10 -0500

    Back up for Migration to AndroidX

--- app/src/main/java/com/example/gobishop/activity/MainActivity.kt
+++ app/src/main/java/com/example/gobishop/activity/MainActivity.kt
@@ -1,7 +1,13 @@
-package com.example.gobishop
+package com.example.gobishop.activity
 
+/**
+ * Created by Yee on 2020/12/24.
+ * Github: Grindewald1900
+ * Email: grindewald1504@gmail.com
+ */
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gobishop.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/gobishop/view/MainView.kt
@@ -0,0 +1,10 @@
+package com.example.gobishop.view
+
+
+/**
+ * Created by Yee on 2020/12/25.
+ * Github: Grindewald1900
+ * Email: grindewald1504@gmail.com
+ */
+interface MainView {
+}
