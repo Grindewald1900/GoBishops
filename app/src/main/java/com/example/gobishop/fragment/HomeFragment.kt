ff875593efa710876d32d529c35e144e288facf1
Yee <17788108425@163.com>
2020-12-30 18:31:14 -0500

    Implemented home page demo

--- app/src/main/java/com/example/gobishop/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/gobishop/fragment/HomeFragment.kt
@@ -1,12 +1,17 @@
 package com.example.gobishop.fragment
 
+import android.graphics.Typeface.BOLD
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.StyleSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.example.gobishop.R
+import com.example.gobishop.utils.TextUtil
 import com.joooonho.SelectableRoundedImageView
 import kotlinx.android.synthetic.main.fragment_home.*
 
@@ -42,5 +47,18 @@ class HomeFragment : Fragment() {
             .asBitmap()
             .load(R.drawable.img_portrait)
             .into(ivPortrait)
+        tv_home_1.text = initText(getString(R.string.f_home_activity))
+        tv_home_2.text = initText(getString(R.string.f_home_activity))
+        tv_home_3.text = initText(getString(R.string.f_home_activity))
+
+    }
+
+    private fun initText(text: String): SpannableString{
+        val spannable = SpannableString(text)
+        var index: ArrayList<Int> = TextUtil.getCharIndex(text, "\n")
+        spannable.setSpan(StyleSpan(BOLD), index[0], index[1], Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannable.setSpan(StyleSpan(BOLD), index[1], index[2], Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannable.setSpan(StyleSpan(BOLD), index[3], index[4], Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        return spannable
     }
 }
---
+++ app/src/main/java/com/example/gobishop/utils/TextUtil.kt
@@ -0,0 +1,25 @@
+package com.example.gobishop.utils
+
+
+/**
+ * Created by Yee on 2020/12/30.
+ * Github: Grindewald1900
+ * Email: grindewald1504@gmail.com
+ */
+class TextUtil {
+    companion object{
+        fun getCharIndex(string: String, target: String): ArrayList<Int>{
+            var indexes: ArrayList<Int> = ArrayList()
+            var tempIndex: Int = 0
+            indexes.add(0)
+            while (tempIndex != -1){
+                tempIndex = string.indexOf(target, tempIndex)
+                if(tempIndex != -1){
+                    indexes.add(tempIndex)
+                    tempIndex++
+                }
+            }
+            return indexes
+        }
+    }
+}
