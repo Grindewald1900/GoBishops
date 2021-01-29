ca244c72751347151289ca686c18f204e65fd5c0
Yee <17788108425@163.com>
2021-01-28 23:49:46 -0500

    [UI] Login Page(version 2) updated

--- app/src/main/java/com/example/gobishops/view/LoginActivity.kt
+++ app/src/main/java/com/example/gobishops/view/LoginActivity.kt
@@ -4,16 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.example.gobishops.R
 import com.example.gobishops.contract.LoginContract
 import com.example.gobishops.presenter.LoginPresenter
-import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_login_v2.*
 
 class LoginActivity : AppCompatActivity(), LoginContract.View{
     private var presenter: LoginContract.Presenter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_login_v2)
         presenter = LoginPresenter(this)
     }
 
@@ -21,10 +22,14 @@ class LoginActivity : AppCompatActivity(), LoginContract.View{
      * View Initialization, including onClickListener()
      */
     override fun initView(){
-        tv_login_register.setOnClickListener {
+        tv_login_register_v2.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
+        Glide.with(this)
+            .asBitmap()
+            .load(R.drawable.img_portrait)
+            .into(iv_login_portrait_v2)
     }
 
     /**
